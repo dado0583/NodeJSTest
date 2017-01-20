e6f2e066e2d903796f894a581b616eebd985848a
dado0583 <davidjdoherty@gmail.com>
2017-01-19 21:00:16 -0500

    Adding header

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var middleware = require('./middleware.js');
 
 app.use(middleware.requireAuthentication);
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
 	res.send('Hello Express');
-});
+});*/
 
 app.get('/about', function(req, res) {
 	res.send('About Us');
